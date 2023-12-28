5398f2ee4756fcb0529d62fd44c1622a90d551bf
Mushal35 <88936844+Mushal35@users.noreply.github.com>
2023-12-28 20:46:56 +0530

    route change

--- client/src/index.js
+++ client/src/index.js
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         element: <Editor />,
       },
       {
-        path: "pricing",
+        path: "plans",
         element: <Plans />,
       },
       {
