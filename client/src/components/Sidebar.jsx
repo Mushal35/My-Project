68225c799b314459129e2a937ea3f91e1cf16bb4
Mushal <mushal9892@student.sfit.ac.in>
2023-12-29 00:00:41 +0530

    Bug fix

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -19,9 +19,9 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
       }}
       onClick={() => setSelected(title)}
       icon={icon}
+      component={<Link to={to} />}
     >
       <Typography>{title}</Typography>
-      <Link to={to} />
     </MenuItem>
   );
 };
