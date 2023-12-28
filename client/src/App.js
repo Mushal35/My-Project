6ef1f8d07c231f0a3b03db580daa905070b9e240
Mushal35 <88936844+Mushal35@users.noreply.github.com>
2023-12-28 20:42:17 +0530

    Bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import Topbar from "./components/Topbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import { Outlet } from "react-router-dom";
 import {useStateContext} from "./contexts/contextProvider"
